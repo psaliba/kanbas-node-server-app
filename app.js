63656a0559a9cd47ed23d9e26494380434b63d18
Peter Saliba <petersaliba23@gmail.com>
2024-04-03 13:19:04 -0400

    up to 4.0

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ import lab5 from "./lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/users/routes.js";
+
+await mongoose.connect("mongodb://127.0.0.1:27017/kanbas", {
+    directConnection: true
+});
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+  }));
 app.use(express.json());
+
+// print every request to console for debugging
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+  });
+
 Hello(app);
 lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
+
+
 
 app.listen(process.env.PORT || 4000, () => console.log("Server is running on port 4000"));
+

bb0ed05855e2bcaa8608eb78f14c2e94e57b8792
Peter Saliba <petersaliba23@gmail.com>
2024-04-04 21:51:48 -0400

    direct connect

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ import UserRoutes from "./Kanbas/users/routes.js";
 import session from "express-session";
 import "dotenv/config";
 
-await mongoose.connect(process.env.DB_CONNECTION_STRING, {
-    directConnection: true
-});
+await mongoose.connect(process.env.DB_CONNECTION_STRING);
 
 const app = express();
 app.use(cors({
